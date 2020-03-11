98f299c7ab2a35769149495978364e4f8e22687d
F_Cup_Chan <bringonthepretty@gmail.com>
2020-03-11 14:54:00 +0300

    upd

--- app/src/main/java/com/example/astolfogaysounds/MainActivity.java
+++ app/src/main/java/com/example/astolfogaysounds/MainActivity.java
@@ -15,19 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView mainImageView;
 
-    private MediaPlayer mediaPlayer1;
-    private MediaPlayer mediaPlayer2;
-    private MediaPlayer mediaPlayer3;
-    private MediaPlayer mediaPlayer4;
-    private MediaPlayer mediaPlayer5;
-    private MediaPlayer mediaPlayer6;
-    private MediaPlayer mediaPlayer7;
-    private MediaPlayer mediaPlayer8;
-    private MediaPlayer mediaPlayer9;
-    private MediaPlayer mediaPlayer10;
-    private MediaPlayer mediaPlayer11;
-    private MediaPlayer mediaPlayer12;
-    private MediaPlayer mediaPlayer13;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,20 +26,6 @@ public class MainActivity extends AppCompatActivity {
 
         random = new Random();
 
-        mediaPlayer1 = MediaPlayer.create(this, R.raw.gay_sound_1);
-        mediaPlayer2 = MediaPlayer.create(this, R.raw.gay_sound_2);
-        mediaPlayer3 = MediaPlayer.create(this, R.raw.gay_sound_3);
-        mediaPlayer4 = MediaPlayer.create(this, R.raw.gay_sound_4);
-        mediaPlayer5 = MediaPlayer.create(this, R.raw.gay_sound_5);
-        mediaPlayer6 = MediaPlayer.create(this, R.raw.gay_sound_6);
-        mediaPlayer7 = MediaPlayer.create(this, R.raw.gay_sound_7);
-        mediaPlayer8 = MediaPlayer.create(this, R.raw.gay_sound_8);
-        mediaPlayer9 = MediaPlayer.create(this, R.raw.gay_sound_9);
-        mediaPlayer10 = MediaPlayer.create(this, R.raw.gay_sound_10);
-        mediaPlayer11 = MediaPlayer.create(this, R.raw.gay_sound_11);
-        mediaPlayer12 = MediaPlayer.create(this, R.raw.gay_sound_12);
-        mediaPlayer13 = MediaPlayer.create(this, R.raw.gay_sound_13);
-
         actionOnClick();
     }
 
@@ -61,55 +35,96 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 switch (random.nextInt(13)){
                     case 0:{
-                        mediaPlayer1.start();
+                        if (!mediaPlayer.isPlaying()){
+                            mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.gay_sound_1);
+                            mediaPlayer.start();
+                        }
                         break;
                     }
                     case 1:{
-                        mediaPlayer2.start();
+                        if (!mediaPlayer.isPlaying()){
+                            mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.gay_sound_2);
+                            mediaPlayer.start();
+                        }
                         break;
                     }
                     case 2:{
-                        mediaPlayer3.start();
+                        if (!mediaPlayer.isPlaying()){
+                            mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.gay_sound_3);
+                            mediaPlayer.start();
+                        }
                         break;
                     }
                     case 3:{
-                        mediaPlayer4.start();
+                        if (!mediaPlayer.isPlaying()){
+                            mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.gay_sound_4);
+                            mediaPlayer.start();
+                        }
                         break;
                     }
                     case 4:{
-                        mediaPlayer5.start();
+                        if (!mediaPlayer.isPlaying()){
+                            mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.gay_sound_5);
+                            mediaPlayer.start();
+                        }
                         break;
                     }
                     case 5:{
-                        mediaPlayer6.start();
+                        if (!mediaPlayer.isPlaying()){
+                            mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.gay_sound_6);
+                            mediaPlayer.start();
+                        }
                         break;
                     }
                     case 6:{
-                        mediaPlayer7.start();
+                        if (!mediaPlayer.isPlaying()){
+                            mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.gay_sound_7);
+                            mediaPlayer.start();
+                        }
                         break;
                     }
                     case 7:{
-                        mediaPlayer8.start();
+                        if (!mediaPlayer.isPlaying()){
+                            mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.gay_sound_8);
+                            mediaPlayer.start();
+                        }
                         break;
                     }
                     case 8:{
-                        mediaPlayer9.start();
+                        if (!mediaPlayer.isPlaying()){
+                            mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.gay_sound_9);
+                            mediaPlayer.start();
+                        }
                         break;
                     }
                     case 9:{
-                        mediaPlayer10.start();
+                        if (!mediaPlayer.isPlaying()){
+                            mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.gay_sound_10);
+                            mediaPlayer.start();
+                        }
                         break;
                     }
                     case 10:{
-                        mediaPlayer11.start();
+                        if (!mediaPlayer.isPlaying()){
+                            mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.gay_sound_1);
+                            mediaPlayer.start();
+                        }
+                        mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.gay_sound_11);
+                        mediaPlayer.start();
                         break;
                     }
                     case 11:{
-                        mediaPlayer12.start();
+                        if (!mediaPlayer.isPlaying()){
+                            mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.gay_sound_12);
+                            mediaPlayer.start();
+                        }
                         break;
                     }
                     case 12:{
-                        mediaPlayer13.start();
+                        if (!mediaPlayer.isPlaying()){
+                            mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.gay_sound_13);
+                            mediaPlayer.start();
+                        }
                         break;
                     }
                 }
@@ -120,18 +135,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mediaPlayer1.release();
-        mediaPlayer2.release();
-        mediaPlayer3.release();
-        mediaPlayer4.release();
-        mediaPlayer5.release();
-        mediaPlayer6.release();
-        mediaPlayer7.release();
-        mediaPlayer8.release();
-        mediaPlayer9.release();
-        mediaPlayer10.release();
-        mediaPlayer11.release();
-        mediaPlayer12.release();
-        mediaPlayer13.release();
+        mediaPlayer.release();
     }
 }
